6e99f85d2e5ce7416931917c08f58cd53f86a442
AhmedQu <ahmed.etrendz@gmail.com>
2021-08-17 16:55:59 +0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-lower-res",
+      "name": "lower-res",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.3742774893135135,
+        "pitch": -0.00733503232410726,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3369020094527997,
+          "pitch": -0.239459451388516,
+          "rotation": 5.497787143782138,
+          "target": "0-lower-res"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
